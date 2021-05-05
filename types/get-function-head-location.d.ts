01acd01c00d0ec974e0dd04b64ef396e1d4e3f59
tyankatsu <frips.ryilsufupe+dev@gmail.com>
2021-05-05 18:56:00 +0900

    feat: build declaration

--- types/get-function-head-location.d.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Get the location of the given function node for reporting.
- * @param {Rule.Node} node - The function node to get.
- * @param {SourceCode} sourceCode - The source code object to get tokens.
- * @returns {string} The location of the function node for reporting.
- */
-export function getFunctionHeadLocation(node: Rule.Node, sourceCode: SourceCode): string;
-import { Rule } from "eslint";
-import { SourceCode } from "eslint";
--- types/get-function-name-with-kind.d.ts
+++
@@ -1,7 +0,0 @@
-/**
- * Get the name and kind of the given function node.
- * @param {Rule.Node} node - The function node to get.
- * @returns {string} The name and kind of the function node.
- */
-export function getFunctionNameWithKind(node: Rule.Node): string;
-import { Rule } from "eslint";
